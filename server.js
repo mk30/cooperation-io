dbddce527685dbb3427c8ed025f7b28c124cc4ec
mk30 <marina.kukso@gmail.com>
2013-10-13 02:50:33 +0000

    fixed directory

--- server.js
+++ server.js
@@ -5,29 +5,29 @@ var article = require('markdown-directory')(__dirname + '/articles');
 
 var server = http.createServer(function (req, res) {
 
-        if(req.url == "/"){
+	if(req.url == "/"){
 
-             fs.readdir(__dirname, function (err, files) {
-                res.end(files.map(function (file) {
-                    var title = file.replace(/\.markdown$/, '');
-                    return '<div><a href="/article/i' + title + '">'
-                        + title + '</a></div>'
-                    ;
-                }).join('\n'));
-            })
-            var m = RegExp('^/article/(.+)').exec(req.url);
-            }
-        else{
-            if (!m) return res.end('cats');
-            res.setHeader('content-type', 'text/html');
-            fs.createReadStream(__dirname + '/article.html')
-                .pipe(hyperstream({
-                    'title': m[1],
-                    '#article': article(m[1]),
-                }))
-                .pipe(res)
-            ;
-            }
+		 fs.readdir(__dirname + '/articles', function (err, files) {
+			res.end(files.map(function (file) {
+				var title = file.replace(/\.markdown$/, '');
+				return '<div><a href="/article/i' + title + '">'
+					+ title + '</a></div>'
+				;
+			}).join('\n'));
+		})
+		var m = RegExp('^/article/(.+)').exec(req.url);
+	}
+	else{
+		if (!m) return res.end('cats');
+		res.setHeader('content-type', 'text/html');
+		fs.createReadStream(__dirname + '/article.html')
+			.pipe(hyperstream({
+				'title': m[1],
+				'#article': article(m[1]),
+			}))
+			.pipe(res)
+		;
+	}
 });
 server.listen(80);
 
